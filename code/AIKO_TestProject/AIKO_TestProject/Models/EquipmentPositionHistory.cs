1b41e358b6fabddefbdbf9a54868384f0edd495e
Luyeye Henri <luyeye.henri@kooli.io>
2021-09-30 16:26:13 +0100

    ADD Equipment Position History Model

---
+++ code/AIKO_TestProject/AIKO_TestProject/Models/EquipmentPositionHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIKO_TestProject.Models
+{
+    [Table("equipment_position_history", Schema = "operation")]
+    public class EquipmentPositionHistory
+    {
+        public Guid equipment_id { get; set; }
+        public int lat { get; set; }
+        public int lon { get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
