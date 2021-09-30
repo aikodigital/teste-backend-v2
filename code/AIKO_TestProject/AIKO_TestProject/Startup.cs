d6c3117aad7d9d7bafee2444005d15015ea8aa1b
Luyeye Henri <luyeye.henri@kooli.io>
2021-09-30 16:26:52 +0100

    Configure Service EquipmentPositionHistoryContext

--- code/AIKO_TestProject/AIKO_TestProject/Startup.cs
+++ code/AIKO_TestProject/AIKO_TestProject/Startup.cs
@@ -43,6 +43,8 @@ namespace AIKO_TestProject
                 .AddDbContext<EquipmentModelContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
             services.AddEntityFrameworkNpgsql()
                 .AddDbContext<EquipmentStateHistoryContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<EquipmentPositionHistoryContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
