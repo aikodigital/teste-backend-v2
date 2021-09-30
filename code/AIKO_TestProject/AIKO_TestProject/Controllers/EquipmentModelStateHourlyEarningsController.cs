ef8a176e35ea14e1fe7f39b30bbb665ba0da6d19
Luyeye Henri <luyeye.henri@kooli.io>
2021-09-30 16:55:06 +0100

    ADD EquipmentModelStateHourlyEarningsController

---
+++ code/AIKO_TestProject/AIKO_TestProject/Controllers/EquipmentModelStateHourlyEarningsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AIKO_TestProject.Context;
+using AIKO_TestProject.Models;
+
+namespace AIKO_TestProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipmentModelStateHourlyEarningsController : ControllerBase
+    {
+        private readonly EquipmentModelStateHourlyEarningsContext _context;
+
+        public EquipmentModelStateHourlyEarningsController(EquipmentModelStateHourlyEarningsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EquipmentModelStateHourlyEarnings
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EquipmentModelStateHourlyEarnings>>> GetEquipmentModelStateHourlyEarnings()
+        {
+            return await _context.EquipmentModelStateHourlyEarnings.ToListAsync();
+        }
+
+        // GET: api/EquipmentModelStateHourlyEarnings/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EquipmentModelStateHourlyEarnings>> GetEquipmentModelStateHourlyEarnings(Guid id)
+        {
+            var equipmentModelStateHourlyEarnings = await _context.EquipmentModelStateHourlyEarnings.FindAsync(id);
+
+            if (equipmentModelStateHourlyEarnings == null)
+            {
+                return NotFound();
+            }
+
+            return equipmentModelStateHourlyEarnings;
+        }
+
+        // PUT: api/EquipmentModelStateHourlyEarnings/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEquipmentModelStateHourlyEarnings(Guid id, EquipmentModelStateHourlyEarnings equipmentModelStateHourlyEarnings)
+        {
+            if (id != equipmentModelStateHourlyEarnings.equipment_model_id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(equipmentModelStateHourlyEarnings).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EquipmentModelStateHourlyEarningsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EquipmentModelStateHourlyEarnings
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<EquipmentModelStateHourlyEarnings>> PostEquipmentModelStateHourlyEarnings(EquipmentModelStateHourlyEarnings equipmentModelStateHourlyEarnings)
+        {
+            _context.EquipmentModelStateHourlyEarnings.Add(equipmentModelStateHourlyEarnings);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EquipmentModelStateHourlyEarningsExists(equipmentModelStateHourlyEarnings.equipment_model_id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEquipmentModelStateHourlyEarnings", new { id = equipmentModelStateHourlyEarnings.equipment_model_id }, equipmentModelStateHourlyEarnings);
+        }
+
+        // DELETE: api/EquipmentModelStateHourlyEarnings/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEquipmentModelStateHourlyEarnings(Guid id)
+        {
+            var equipmentModelStateHourlyEarnings = await _context.EquipmentModelStateHourlyEarnings.FindAsync(id);
+            if (equipmentModelStateHourlyEarnings == null)
+            {
+                return NotFound();
+            }
+
+            _context.EquipmentModelStateHourlyEarnings.Remove(equipmentModelStateHourlyEarnings);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EquipmentModelStateHourlyEarningsExists(Guid id)
+        {
+            return _context.EquipmentModelStateHourlyEarnings.Any(e => e.equipment_model_id == id);
+        }
+    }
+}
