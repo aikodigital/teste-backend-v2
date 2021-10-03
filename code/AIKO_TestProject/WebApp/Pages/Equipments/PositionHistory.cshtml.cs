ba1d8e7d0fa52204232512ecd5298f05162e683f
Luyeye Henri <luyeye.henri@kooli.io>
2021-10-03 21:34:14 +0100

    Add Features

---
+++ code/AIKO_TestProject/WebApp/Pages/Equipments/PositionHistory.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AIKO_TestProject.Context;
+using WebApp.Services;
+using WebApp.Extras;
+
+namespace WebApp.Pages.Equipments
+{
+    public class PositionHistoryModel : PageModel
+    {
+
+        public IList<EquipmentPositionLastHistory> EquipmentPositionLastHistory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var client = new Client(Helper.APIBaseUrl, new System.Net.Http.HttpClient());
+            var result = await client.AllAsync((Guid)id);
+            EquipmentPositionLastHistory = result;
+
+            if (EquipmentPositionLastHistory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ code/AIKO_TestProject/WebApp/Pages/Equipments/StateHistory.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using AIKO_TestProject.Context;
+using WebApp.Services;
+using WebApp.Extras;
+
+namespace WebApp.Pages.Equipments
+{
+    public class StateHistoryModel : PageModel
+    {
+        public IList<EquipmentStateLastHistory> EquipmentStateLastHistory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var client = new Client(Helper.APIBaseUrl, new System.Net.Http.HttpClient());
+            var result = await client.All2Async((Guid)id);
+
+            EquipmentStateLastHistory = result;
+
+            if (EquipmentStateLastHistory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
