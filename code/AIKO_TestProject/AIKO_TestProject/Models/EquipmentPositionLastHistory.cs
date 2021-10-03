76ab1d8860833b4e134bf06c7a8657218d89171e
Luyeye Henri <luyeye.henri@kooli.io>
2021-10-03 15:00:13 +0100

    New Features

---
+++ code/AIKO_TestProject/AIKO_TestProject/Models/EquipmentPositionLastHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AIKO_TestProject.Models
+{
+    public class EquipmentPositionLastHistory
+    {
+        public DateTime equipment_position_date { get; set; }
+        public Single equipment_position_lat { get; set; }
+        public Single equipment_position_lon { get; set; }
+        public Guid equipment_id { get; set; }
+        public string equipment_name { get; set; }
+        public Guid equipment_model_id { get; set; }
+        public string equipment_model_name { get; set; }
+    }
+}
--- code/AIKO_TestProject/AIKO_TestProject/Startup.cs
+++ code/AIKO_TestProject/AIKO_TestProject/Startup.cs
@@ -51,7 +51,8 @@ namespace AIKO_TestProject
                 .AddDbContext<EquipmentPositionHistoryContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
             services.AddEntityFrameworkNpgsql()
                     .AddDbContext<EquipmentModelStateHourlyEarningsContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
-
+            services.AddEntityFrameworkNpgsql()
+        .AddDbContext<EquipmentStateLastHistoryContext>(options => options.UseNpgsql(Configuration.GetConnectionString("AIKODB")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
