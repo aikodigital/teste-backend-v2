5046bca30d2ccff78d742eaddfaa1220726816ca
Eurico Mazivila <euricoinaciomazive@gmail.com>
2021-10-04 23:10:52 +0200

    fix: added EquipmentModelStateHourlyEarning cs file

---
+++ Aiko_Digital_Razor_Pages/Aiko_Digital_Razor_Pages/Pages/Features/EquipmentModelStateHourlyEarning/EquipmentModelStateHourlyEarning.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aiko_Digital_Razor_Pages.Pages.Features.EquipmentModelStateHourlyEarning
+{
+    public class EquipmentModelStateHourlyEarning : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
