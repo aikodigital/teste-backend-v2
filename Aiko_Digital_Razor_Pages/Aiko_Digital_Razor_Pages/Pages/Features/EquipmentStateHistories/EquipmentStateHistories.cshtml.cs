f0025ead4a46547465d7442c70fc57900380fbc9
Eurico Mazivila <euricoinaciomazive@gmail.com>
2021-10-04 23:09:30 +0200

    feat: implemented kendo ui in all entity for list data

--- Aiko_Digital_Razor_Pages/Aiko_Digital_Razor_Pages/Pages/Features/EquipmentStateHistories/EquipmentStateHistories.cshtml.cs
+++ Aiko_Digital_Razor_Pages/Aiko_Digital_Razor_Pages/Pages/Features/EquipmentStateHistories/EquipmentStateHistories.cshtml.cs
@@ -5,8 +5,6 @@ namespace Aiko_Digital_Razor_Pages.Pages.Features.EquipmentStateHistories
     public class EquipmentStateHistoriesModel : PageModel
     {
         
-        
-        
         public void OnGet()
         {
             
