df7abfd94a3579bd0e591c7cebe4ec63e4e58758
Luyeye Henri <luyeye.henri@kooli.io>
2021-10-04 10:04:41 +0100

    ADD TestClasses

---
+++ code/AIKO_TestProject/UnitTests/Extras/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Extras
+{
+    public class Helper
+    {
+        public static string APIBaseUrl { get; set; } = "https://localhost:44355/";
+    }
+}
---
+++ code/AIKO_TestProject/UnitTests/Tests/EquipmentPositionLastHistory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.Extras;
+using UnitTests.Services;
+
+namespace UnitTests.Tests
+{
+    [TestClass]
+    public class EquipmentPositionLastHistory_TestClass
+    {
+        public IList<EquipmentPositionLastHistory> EquipmentPositionLastHistory { get; set; }
+
+        [TestMethod]
+        public async Task EquipmentPositionLastHistories_ByIDAsync()
+        {
+            var client = new Client(Helper.APIBaseUrl, new System.Net.Http.HttpClient());
+            var result = await client.EquipmentPositionLastHistoriesAsync(Guid.Parse("fe2a2e11-bfa6-46b6-990b-fd8175946b7e"));
+
+            EquipmentPositionLastHistory = result;
+
+            Assert.IsNotNull(EquipmentPositionLastHistory);
+        }
+
+        [TestMethod]
+        public async Task EquipmentPositionLastHistories_AllAsync()
+        {
+            var client = new Client(Helper.APIBaseUrl, new System.Net.Http.HttpClient());
+            var result = await client.AllAsync(Guid.Parse("fe2a2e11-bfa6-46b6-990b-fd8175946b7e"));
+
+            EquipmentPositionLastHistory = result;
+
+            Assert.IsNotNull(EquipmentPositionLastHistory);
+        }
+    }
+}
